89b5a48f685f1eb5bb57833fdefb6b8373f7f0ce
Shun Furukawa <furukawa9312@gmail.com>
2020-10-05 18:11:41 +0900

    ADD create Post form

--- front/src/views/Post.vue
+++ front/src/views/Post.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
     <h1>This page is Post Index.</h1>
+    <form v-on:submit.prevent="create">
+      <label>name</label>
+      <input type="text" v-model="name">
+      <button>CREATE</button>
+    </form>
     <ul>
       <li v-for="post in posts" :key="post.id">{{ post.name }}</li>
     </ul>
@@ -23,6 +28,18 @@
       getPosts() {
         this.$axios.get("/posts").then((res) => {
           this.posts = res.data
+        }).catch((error) => {
+          console.log(error);
+        })
+      },
+      create: function() {
+        this.$axios.post("/posts", {
+          post: {
+            name: this.name
+          }
+        }).then((res) => {
+          this.posts.push(res.data);
+          this.name = null;
         }).catch((error) => {
           console.log(error);
         })
