9b62b3e955ba998e7fe431515e33699a009c717e
Shun Furukawa <furukawa9312@gmail.com>
2020-10-05 16:11:46 +0900

    ADD seed of post data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.time do |i|
+  Post.create(name: "Post #{i+1}")
+end
