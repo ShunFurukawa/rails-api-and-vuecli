dd5a5568bcd2d5a628f162386b4275aebed895f1
Shun Furukawa <furukawa9312@gmail.com>
2020-10-05 18:03:16 +0900

    ADD Post Index

---
+++ front/src/views/Post.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>This page is Post Index.</h1>
+    <ul>
+      <li v-for="post in posts" :key="post.id">{{ post.name }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Post",
+    data: function() {
+      return {
+        posts: [],
+        name: null
+      }
+    },
+    created: function() {
+      this.getPosts();
+    },
+    methods: {
+      getPosts() {
+        this.$axios.get("/posts").then((res) => {
+          this.posts = res.data
+        }).catch((error) => {
+          console.log(error);
+        })
+      }
+    }
+  };
+</script>
---
+++ front/vue.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  devServer: {
+    proxy: "http://ruby:3000"
+  }
+};
